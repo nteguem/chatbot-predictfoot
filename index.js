9cf0b0aea986aa2d0d0365f9ee76045614fbd2a3
Mercure Mekinda <mercuremekinda@gmail.com>
2023-08-16 16:05:09 +0100

    add

--- index.js
+++ index.js
@@ -36,14 +36,14 @@ app.use((req, res, next) => {
   next();
 });
 
-// Create instance whatapp
-const client = initializeWhatsAppClient();
+// // Create instance whatapp
+// const client = initializeWhatsAppClient();
 
-//Handle incoming messages from the chatbot using the modular function.
-handleIncomingMessages(client);
+// //Handle incoming messages from the chatbot using the modular function.
+// handleIncomingMessages(client);
 
-// Launch WhatsApp client
-client.initialize();
+// // Launch WhatsApp client
+// client.initialize();
 
 
 // App Routes
